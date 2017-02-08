5260b6049d48e780f7ed6764f5e414bdafcb2631
jsmx2025 <JSMX_1996@wireless-s0-so-150-39-230.uchicago.edu>
2017-02-08 09:55:26 -0600

    final
    final

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,35 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id]} )
+  end
 
+  def new_form
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("http://localhost:3000")
+  end
+
+  def destroy
+    @delete = Photo.destroy(params[:id])
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id]} )
+  end
+
+  def update_row
+    u = Photo.find_by({ :id => params[:id]} )
+    u.source = params[:the_new_source]
+    u.caption = params[:the_new_caption]
+    u.save
+    redirect_to("http://localhost:3000/photos/#{params[:id]}")
+end
 end
